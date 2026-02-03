ca61844fb89b843274240216c9511139a7c247af
Vamshi Kumar <vamshigvk143@gmail.com>
2026-02-03 19:52:46 +0530

    Update HomeController.java

--- maven-app/src/main/java/com/example/maven_app/HomeController.java
+++ maven-app/src/main/java/com/example/maven_app/HomeController.java
@@ -1,1 +1,14 @@
+package com.example.my_maven_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot Maven App CI/CD using Jenkins + Docker Swarm 🚀";
+    }
+}
 
